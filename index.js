70920b4d35e0009963b54137f34cba562e3acd46
thrillim <86657389+thrillim@users.noreply.github.com>
2025-07-28 22:44:46 +0700

    solve

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+var multer = require('multer');
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html')
 });
 
-
+app.post('/api/fileanalyse', (req, res) => {
+  const upload = multer({ dest: 'uploads/' });
+  upload.single('upfile')(req, res, function (err) {
+    if (err) {
+      return res.status(400).send({ error: 'File upload failed' });
+    }
+    if (!req.file) {
+      return res.status(400).send({ error: 'No file uploaded' });
+    }
+    const fileInfo = {
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    };
+    res.json(fileInfo);
+  });
+})
 
 
 const port = process.env.PORT || 3000;
